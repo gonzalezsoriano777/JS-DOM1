29b7b26e3c5d276f82d87424e4f24e378d7fe4a2
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-08-08 02:54:40 +0000

    lesson 2 code

---
+++ app.js
@@ -0,0 +1,13 @@
+var banner = document.getElementById('page-banner')
+var booklist = document.getElementById('book-list')
+
+bookList
+
+// Calling an ID using "document.getElementByID" and then using the parenthesis "()" to calling the ID so it can be shown and displayed in the screen 
+
+
+
+const search = document.getElementById('search-books');
+const bookList = document.getElementById('book-list');
+
+console.log(search, bookList);
