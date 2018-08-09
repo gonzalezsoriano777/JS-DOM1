380ab5f36d624bfe5c3371c30c866edca9b39f21
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-08-09 22:52:05 +0000

    lesson5

--- app.js
+++ app.js
@@ -52,3 +52,12 @@ Array.from(books).forEach(fucntion(book){
   console.log(book);
 });
 
+// Tutorial 5 - Changing text and HTML Content
+
+Array.from(books).forEach(function(book){
+  book.textContent += '(book title)';
+});
+
+const bookList = document.querySelector('#book-list');
+//bookList.innerHTML = '<h2>Book and more books...</h2>';
+bookList.innerHTML += '<p>This is how you add HTML</p>';
