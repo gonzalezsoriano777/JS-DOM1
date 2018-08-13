5ce67fbcac4970ced247c04bd57f23c58435360d
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-08-13 23:31:49 +0000

    lesson11

--- app.js
+++ app.js
@@ -131,3 +131,17 @@ list.addEventListener('click', fucntion(e){
     list.removeChild(li);
   }
 })
+
+// Tutorial 11 - Interacting with Forms
+
+// add book-list
+
+const addForms = document.forms('add-book');
+
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  const value = addForm.querySelector('input[type="text"]').value;
+  console.log(value);
+  
+});
+
