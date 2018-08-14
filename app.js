fcbdd29d1ee392748be555ba683915a584ac387b
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-08-14 22:11:50 +0000

    lesson18

--- app.js
+++ app.js
@@ -249,5 +249,81 @@ tabs.addEventListener('click', function(e) {
     }
 })
 
+// Tutorial  11... 18 - DOMContentLoaded Event
 
+// Make the DOM be fully loaded ..
+document.addEventListener('DOMContentLoaded', function(){
+  
+}
+
+//delete books
+const list = document.querySelector('#book-list ul');
+>>>>>>> lesson10
+
+list.addEventListener('click', fucntion(e){
+  if(e.target.className == 'delete') {
+    const li = e.target.parentElement;
+    list.removeChild(li);
+  }
+})
+
+
+
+// add book-list
+
+const addForms = document.forms('add-book');
+
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  const value = addForm.querySelector('input[type="text"]').value;
+  console.log(value);
+  
+// hide books 
+
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', function(e){
+  if(hideBox.checked){
+    list.style.display = "none";
+  }else {
+    list.style.display = "none"
+  }else {
+    list.style.display = "initial"
+  }
+});
+
+
+
+//filter books
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', function(e){
+  const term = e.target.value.toLowerCase();
+  const books = list.getElementsByTagName('li');
+  Array.from(books).forEach(function(book) {
+      const title = book.firstElementChild.textContent;
+      if(title.toLowerCase().indexOf(term) !=-1){
+        book.style.display = 'block';
+      }else {
+        book.style.display = 'none';
+        
+      }
+  })
+})
+
+
+
+// tabbed content
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel')
+tabs.addEventListener('click', function(e) {
+    id(e.target.tagName == "LI") {
+      const targetPanel = document.querySelector(e.target.dataset.target);
+      panels.forEach(function(panel){
+        if(panel == targetPanel){
+          panel.classList.add('active');
+        }else{
+          panel.classList.remove('active');
+        }
+      })
+    }
+})
 
